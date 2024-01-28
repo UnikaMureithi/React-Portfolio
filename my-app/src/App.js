1e167f0106b569f0ec36393994b086275666c001
Unika Mureithi <unikamureithi@gmail.com>
2024-01-28 17:23:59 +0300

    feat: add skills component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,17 @@
 // import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 // import React, { useEffect } from 'react';
 import Header from './components/Header';
 import About from './components/About';
+import Skills from './components/Skills';
+
 function App() {
   return (
     <div id="home">
     <Header />
     <About />
+    <Skills/>
   </div>
 
   );
---
+++ my-app/src/components/Skills.js
@@ -0,0 +1,52 @@
+function Skills() {
+    return (
+<div className="skills container">
+      <div className="section-title">
+        <h2>Skills</h2>
+      </div>
+
+      <div>
+        <ul>
+          <li>
+            <i className="material-icons">chevron_right</i>
+            <strong>WEB DEVELOPMENT:</strong> I have been able to develop a few
+            web applications in some university projects. These websites were
+            developed mainly using HTML, CSS and JavaScript.
+          </li>
+          <br />
+          <br />
+          <br />
+          <li>
+            <i className="material-icons">chevron_right</i>
+            <strong>PYTHON:</strong> I undertook a project that was developed
+            using the Rapid Application Development Methodology (RAD). This app
+            enabled users to provide input data in a form, where they could
+            then receive a prediction of their susceptibility/likelihood of
+            having cardiovascular disease. The tools used were Python, Django
+            framework, Jupyter Notebook, CSS and HTML.
+          </li>
+          <br />
+          <br />
+          <br />
+          <br />
+          <li>
+            <i className="material-icons">chevron_right</i>
+            <strong>FRAMEWORKS:</strong> Laravel, CodeIgniter and Django are
+            frameworks that I am familiar with using, and they provide a
+            structured and predefined way of organizing code.
+          </li>
+          <br />
+          <br />
+          <br />
+          <li>
+            <i className="material-icons">chevron_right</i>
+            <strong>CHART JS:</strong> It is a helpful JavaScript library that
+            enables me to create interactive and visually appealing charts and
+            graphs on web pages, which is further used for data visualization.
+          </li>
+        </ul>
+      </div>
+    </div>
+    );
+}
+export default Skills;
